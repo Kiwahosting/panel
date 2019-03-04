929c418449e3e1de555c886f34b32eef039a2e6e
Dave <dave@davecode.me>
2019-03-04 18:40:04 -0500

    add a 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { withStyles, Typography } from '@material-ui/core';
+
+import MainLayout from 'layouts/MainLayout';
+import { SEO, Link } from 'components';
+
+// eslint-disable-next-line no-unused-vars
+const styles = (theme) => ({
+
+});
+
+function Home(props) {
+  const { classes: c } = props;
+
+  return <MainLayout {...props}>
+    <SEO title='Kiwahosting' isRoot />
+    <Typography component='h1' variant='h2' align='center'>
+      404 Not Found
+    </Typography>
+    <Typography component='p' variant='body1' align='center'>
+      Go to the <Link to='/'>homepage</Link>
+    </Typography>
+  </MainLayout>;
+}
+
+import withRoot from 'withRoot';
+export default withRoot(withStyles(styles)(Home));
