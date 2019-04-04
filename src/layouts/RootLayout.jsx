9e3c42715f3d6d4dc3b8562e5258e4a399a5cf4f
Dave Caruso <dave@davecode.me>
2019-04-03 21:04:40 -0400

    Fix SOME of the SSR issues

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import posed, { PoseGroup } from 'react-pose';
 import AuthLayout from './AuthLayout';
 import withRoot from 'withRoot';
@@ -6,11 +6,11 @@ import withRoot from 'withRoot';
 const RoutesContainer = posed.div({
   enter: {
     opacity: 1,
-    transition: { duration: 200 },
+    transition: ({ animate }) => ({ duration: animate ? 200 : 0 }),
   },
   exit: {
     opacity: 0,
-    transition: { duration: 200 },
+    transition: ({ animate }) => ({ duration: animate ? 200 : 0 }),
   },
 });
 const ContentContainer = posed.div({
@@ -23,8 +23,45 @@ const ContentContainer = posed.div({
 function Layout({ children, location, pageContext: { layout } }) {
   
   if (layout === 'auth') {
+    if(typeof document === 'undefined') {
+      return <AuthLayout>
+        <div
+          style={{
+            position: 'relative',
+            transition: 'height 0s',
+            width: '100%',
+            overflow: 'hidden',
+          }}>
+          <RoutesContainer
+            key={location.key}
+            style={{
+              width: '100%',
+              position: 'absolute',
+              left: '0px',
+              opacity: '1',
+              transform: 'none',
+              transformOrigin: '0% 50% 0px',
+            }}
+          >
+            {children}
+          </RoutesContainer>
+        </div>
+      </AuthLayout>;
+    }
+
     const [height, setHeight] = useState(undefined);
     const [animate, setAnimate] = useState(false);
+    const [extra, setExtra] = useState(0);
+    
+    useLayoutEffect(() => {
+      setTimeout(() => {
+        setExtra(1);
+      }, 200);
+      setTimeout(() => {
+        setExtra(2);
+      }, 200);
+    }, []);
+
     return <AuthLayout>
       <ContentContainer
         pose='visible'
@@ -38,8 +75,9 @@ function Layout({ children, location, pageContext: { layout } }) {
         }}>
         <PoseGroup>
           <RoutesContainer
-            key={location.key}
-            style={{ width: '100%', position: 'absolute' }}
+            key={location.key + extra}
+            poseKey={animate}
+            style={{ width: '100%', position: 'absolute', left: '0' }}
             ref={elem => {
               if (!elem) return;
               if (height !== undefined) setAnimate(true);
