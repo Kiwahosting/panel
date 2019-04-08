65ec08c75183c69520de45de896bba90239a5d27
Dave Caruso <dave@davecode.me>
2019-04-07 20:01:19 -0400

    docs magic

--- src/templates/Documentation.jsx
+++ src/templates/Documentation.jsx
@@ -10,17 +10,24 @@ import {
 
 import DocsLayout from 'layouts/DocsLayout';
 import Link from 'components/Link';
+import IC from 'components/IC';
 
 // eslint-disable-next-line no-unused-vars
 const styles = (theme) => createStyles({
-
+  para: {
+    'li > &': {
+      marginBottom: 0,
+    },
+  },
 });
+
+let c;
 
 const transform = {
   h1: (props) => <Typography variant='h3' paragraph component='h1' {...props} />,
   h2: (props) => <Typography variant='h4' paragraph component='h2' {...props} />,
   h3: (props) => <Typography variant='h5' paragraph component='h3' {...props} />,
-  p: (props) => <Typography variant='body1' paragraph component='p' {...props} />,
+  p: (props) => <Typography variant='body1' paragraph component='p' {...props} className={c.para} />,
   a: (props) => <Link
     {...props}
     to={
@@ -30,12 +37,17 @@ const transform = {
     }
     href={undefined}
   />,
+  // ul: (props) => <ul {...props} style={{ marginTop: 0 }}/>,
+  // ol: (props) => <ol {...props} style={{ marginTop: 0 }}/>,
+  li: (props) => <Typography variant='body1' component='span'><li {...props} /></Typography>,
+  code: IC,
 };
 
 function Documentation(props) {
-  const { data: { markdownRemark: page } } = props;
+  const { data: { markdownRemark: page }, classes, ...rest } = props;
+  c = classes;
 
-  return <DocsLayout {...props}>
+  return <DocsLayout {...rest}>
     {convert(page.html, {
       transform,
     })}
